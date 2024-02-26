dae143aa5f82f13f1d28030f2247fc1670f9700c
JhonathanSilva2 <jhonathancontato4@gmail.com>
2024-02-26 15:27:26 -0300

    Update org-control.tsx

--- trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
+++ trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/_components/org-control.tsx
@@ -4,9 +4,13 @@ import { useOrganizationList } from "@clerk/nextjs"
 import { useParams } from "next/navigation"
 import { use, useEffect } from "react"
 
+// This component is designed to control the active organization in an application,
+// so if you copy and past a manual organization id into the URL, 
+// it will automatically change
 export const OrgControl = () => {
     const params = useParams()
     const {setActive} = useOrganizationList()
+
     useEffect(() => {
         if(!setActive) return
         setActive({
