7bb4876eddb70037de174173138d3aaf4ccf2a63
JhonathanSilva2 <jhonathancontato4@gmail.com>
2024-02-26 15:27:38 -0300

    Update page.tsx

--- trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ trello-clone/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,12 +1,9 @@
 import { OrganizationSwitcher, auth } from "@clerk/nextjs";
 
 const OrganizationIdPage = () => {
-    const {userId, orgId} = auth()
     return (
         <div>
-            <OrganizationSwitcher
-            hidePersonal        
-            />
+            Organization Page
         </div>
     )
 }
